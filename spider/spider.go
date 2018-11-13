4bbd0d6e7b16034d18f0139320d816a9a04aa905
Syd Xu <prof.syd.xu@gmail.com>
2018-11-13 20:18:18 +0800

    unlock sogou

--- spider/spider.go
+++ spider/spider.go
@@ -12,6 +12,7 @@ import (
 	"github.com/yizenghui/wxarticle2md"
 	"html"
 	"log"
+	"net/http"
 	"net/url"
 	"regexp"
 	"strconv"
@@ -41,6 +42,7 @@ type Spider struct {
 	slackBot   *Slack
 	proxy      *Proxy
 	httpClient *grequests.Session
+	cookies    []*http.Cookie
 }
 
 func New(slackBot *Slack, redisClient *redis.Pool, proxyApiKey string) *Spider {
@@ -144,19 +146,12 @@ func (this *Spider) GetGzhArticles(wechatName string) ([]Article, error) {
 }
 
 func (this *Spider) getProfile(name string) (string, error) {
-	var ro *grequests.RequestOptions
-	proxyUrl, _ := this.proxy.Get()
-	if proxyUrl != nil {
-		ro = &grequests.RequestOptions{
-			Proxies: map[string]*url.URL{"https": proxyUrl},
-		}
-	}
-	resp, err := this.httpClient.Get("http://weixin.sogou.com/weixin?type=1&query="+name+"&ie=utf8&_sug_=n&_sug_type_=", ro)
+	resp, err := this.sogouOpenUnlock("http://weixin.sogou.com/weixin?type=1&query="+name+"&ie=utf8&_sug_=n&_sug_type_=", "http://weixin.sogou.com")
 	if err != nil {
 		return "", err
 	}
-	doc, err := goquery.NewDocumentFromResponse(resp.RawResponse)
-
+	reader := bytes.NewReader(resp.Bytes())
+	doc, err := goquery.NewDocumentFromReader(reader)
 	if err != nil {
 		return "", err
 	}
@@ -205,6 +200,125 @@ func (this *Spider) getArticle(link string, referrer string) (string, error) {
 	}
 	mk := wxarticle2md.Convert(a)
 	return strings.TrimSpace(mk), err
+}
+
+func (this *Spider) sogouOpenUnlock(link, referrer string) (*grequests.Response, error) {
+	ro := &grequests.RequestOptions{
+		Headers: map[string]string{
+			"Referer": referrer,
+		},
+	}
+	if this.cookies != nil {
+		ro.Cookies = this.cookies
+	}
+	proxyUrl, _ := this.proxy.Get()
+	if proxyUrl != nil {
+		ro.Proxies = map[string]*url.URL{"https": proxyUrl}
+	}
+	resp, err := this.httpClient.Get(link, ro)
+	if err != nil {
+		return nil, err
+	}
+	body := resp.String()
+	respUrl := resp.RawResponse.Request.URL.String()
+	if strings.Contains(respUrl, "antispider") || strings.Contains(body, "请输入验证码") {
+		sessionCookies := resp.RawResponse.Cookies()
+		snuid, err := this.tryUnlockSogou(respUrl)
+		if err != nil {
+			return nil, err
+		} else {
+			var cookies []*http.Cookie
+			for _, c := range sessionCookies {
+				if c.Name == "SUID" {
+					cookies = append(cookies, &http.Cookie{
+						Name:   "SUV",
+						Value:  c.Value,
+						Domain: "sogou.com",
+					})
+				}
+			}
+			cookies = append(cookies, &http.Cookie{
+				Name:   "SNUID",
+				Value:  snuid,
+				Domain: "sogou.com",
+			})
+			this.cookies = cookies
+			return this.sogouOpenUnlock(link, referrer)
+		}
+	}
+	return resp, nil
+}
+
+func (this *Spider) tryUnlockSogou(referrer string) (string, error) {
+	var ro *grequests.RequestOptions
+	proxyUrl, _ := this.proxy.Get()
+	if proxyUrl != nil {
+		ro = &grequests.RequestOptions{
+			Proxies: map[string]*url.URL{"https": proxyUrl},
+		}
+	}
+	resp, err := this.httpClient.Get(fmt.Sprintf("http://weixin.sogou.com/antispider/util/seccode.php?tc=%d", time.Now().UnixNano()), ro)
+	if err != nil {
+		return "", err
+	}
+	return this.unlockSogouVerify(referrer, resp.Bytes())
+}
+
+func (this *Spider) unlockSogouVerify(referrer string, image []byte) (snuid string, err error) {
+	log.Println("Try to unlock Sogou")
+	file, err := this.slackBot.UploadFile(image)
+	if err != nil {
+		return "", err
+	}
+	defer this.slackBot.DeleteFile(file.ID)
+	log.Println("Uploaded file: ", file.ID)
+	checkTicker := time.NewTicker(2 * time.Second)
+	codeCh := make(chan string, 1)
+	defer close(codeCh)
+	for {
+		select {
+		case <-checkTicker.C:
+			finfo, _, err := this.slackBot.GetFile(file.ID)
+			if err != nil {
+				break
+			} else if finfo.Title != finfo.Name {
+				checkTicker.Stop()
+				codeCh <- finfo.Title
+			}
+		case code := <-codeCh:
+			log.Println("get code: ", code)
+			u, _ := url.Parse(referrer)
+			query := u.Query()
+			ro := &grequests.RequestOptions{
+				Data: map[string]string{
+					"c": code,
+					"v": "5",
+					"r": url.QueryEscape(query.Get("from")),
+				},
+				Headers: map[string]string{
+					"Referer": referrer,
+				},
+			}
+			resp, err := this.httpClient.Post("http://weixin.sogou.com/antispider/thank.php", ro)
+			if err != nil {
+				return "", err
+			}
+			log.Println(resp.String())
+			if resp.Ok {
+				var ret SogouUnlockResponse
+				err := json.Unmarshal(resp.Bytes(), &ret)
+				if ret.Code == 0 && ret.Id != "" {
+					return ret.Id, nil
+				} else if err != nil {
+					return "", err
+				}
+				return "", errors.New(ret.Msg)
+			} else {
+				return "", errors.New("verfiy code failed")
+			}
+		}
+	}
+	return "", nil
 }
 
 func (this *Spider) wxOpenUnlock(link string, referrer string) (*grequests.Response, error) {
@@ -293,4 +407,10 @@ func (this *Spider) unlockWxVerify(link string, image []byte) error {
 		}
 	}
 	return nil
+}
+
+type SogouUnlockResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Id   string `json:"id"`
 }
